15be5779ad7017ab1147f5eb2e43fa99df9211e9
Mieye Ombe <mieye@usophy.com>
2019-11-07 11:53:32 +0100

    Demo of story 1 to be improved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/SubjectAndGrade/Title';
+import SubjectAndGrade from './components/SubjectAndGrade';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      { /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -14,11 +16,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noref
         </a>
-      </header>
+      </header> */ }
+      <Title />
+      <SubjectAndGrade subject="Math" grade="10"  details= "29/10/2019 9.0, 23/09/2019 8.0, 09/08/2019 9.0, 02/10/2019 7.0" />
+      <SubjectAndGrade subject="Physics" grade="9" details= "27/10/2019 6.0, 28/09/2019 8.5, 11/08/2019 9.0, 22/10/2019 7.0" />
+      <SubjectAndGrade subject="Chemistry" grade="8" details= "23/10/2019 9.0, 19/09/2019 8.0, 05/08/2019 9.0, 18/10/2019 7.0" />
     </div>
   );
 }
