3034b5b0eaa66c95c2bdba1049498540826dab1c
antoniocastoS262622 <s262622@studenti.polito.it>
2019-11-07 10:46:14 +0100

    Added some structure on frontend and Router has been implemented

---
+++ frontend/src/components/officer/officer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Officer extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Officer app</h1>
+            </div>
+        );
+    }
+}
+
+export default Officer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import App from './App';
+import Admin from './components/admin/admin';
+import Officer from './components/officer/officer';
+import Teacher from './components/teacher/teacher';
+import Parent from './components/parent/parent';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const routing = (
+    <Router>
+        <Switch>
+            <Route exact path='/' render={() => <App />} />
+            <Route path="/teacher" render={(props) => <Teacher {...props} />} />
+            <Route path="/parent" render={(props) => <Parent {...props} />} />
+            <Route path="/officer" render={(props) => <Officer {...props} />} />
+            <Route path='/admin' render={(props) => <Admin {...props} />} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
