ee7c07238abfd3a8e32a4495c8a3fbbf4bed29b7
Emile <legendre.emile.27@gmail.com>
2019-11-08 20:31:46 +0100

    Add CORS to requests

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,17 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'], // an array of origins or 'ignore'
+                headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers'
+                exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
+                additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
+                maxAge: 60 * 60 * 1000 * 30,
+                credentials: true // boolean - 'Access-Control-Allow-Credentials'
+            }
+        }
     });
 
     await server.register(require('@hapi/cookie'));
