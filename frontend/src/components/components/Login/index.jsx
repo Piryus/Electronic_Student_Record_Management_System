c08c20dd29192df38cf34296ddf593dbc43ad148
antoniocastoS262622 <s262622@studenti.polito.it>
2019-11-09 14:11:15 +0100

    login styled

--- frontend/src/components/components/Login/index.jsx
+++ frontend/src/components/components/Login/index.jsx
@@ -14,11 +14,11 @@ class Login extends Component {
 
    render() {
     return (
-      <div>
-        <form>
+      <div className= "loginContainer">
+        <form className = "loginForm">
           <input type= "text" name="email" placeholder= "Email address" className= "loginName" onChange = {(e) => this.setState({username: e.target.value})} required /><br/>
           <input type= "password" name= "password" placeholder= "Password" className= "loginPassword" onChange={(e) => this.setState({password: e.target.value})} required /><br/><br/>
-          <button onClick = {() => this.props.handleLogin(this.state.username, this.state.password)}>Login</button>
+          <button onClick = {() => this.props.handleLogin(this.state.username, this.state.password) } className="loginButton">Login</button>
         </form>
  
       </div>
