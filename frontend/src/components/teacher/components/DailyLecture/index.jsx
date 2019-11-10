7ea5881b06059f646a6e9c73549ecf16baa7c5a8
antoniocastoS262622 <s262622@studenti.polito.it>
2019-11-10 18:08:36 +0100

    Story 2 frontend with style to be improved

--- frontend/src/components/teacher/components/DailyLecture/index.jsx
+++ frontend/src/components/teacher/components/DailyLecture/index.jsx
@@ -3,14 +3,21 @@ import './style.sass';
 
 class DailyLecture extends Component {
 
-  state = { 
-    date: "",
-    subject: "",
-    summary: "",
-  };
+  constructor(props){
+    super(props);
+    this.state = { 
+      date: "",
+      subject: "",
+      summary: "",
+      classYear: "",
+      classSection: "",
+    };
+  }
+
   
 
   validateData = (event) => {
+
     var d= new Date();
     var d2= new Date(this.state.date);
     var lastSunday= new Date(d.toString());
@@ -24,24 +31,55 @@ class DailyLecture extends Component {
     if(this.state.subject === ""){
       error += "You must fill the Subject \n";
     }
+    if(this.state.classYear === "" || this.state.classSection === ""){
+      error += "You must fill the both Class year and Class section \n";
+    }
     if(this.state.summary === ""){
       error += "You must fill the Lecture summary \n";
     }
-    if(this.state.date === "" || this.state.subject === "" || this.state.summary === ""){
+    if(this.state.date === "" || this.state.subject === "" || this.state.summary === "" || this.state.classYear === "" || this.state.classSection === ""){
       alert(error);
     }
-
-    if(d2 > d){
-      alert("You cannot record a future date");
-      event.preventDefault(); 
-      return; 
-    }
-    if(d2 < lastSunday){
-      alert("It is too late to insert record for the selected");
-      event.preventDefault(); 
-      return;
-    }
-
+    else{
+      if(isNaN(this.state.classYear)){
+        alert('Pleas insert a correct year');
+      }
+      else{
+        if(this.state.classYear <= 0){
+          alert('Pleas insert a correct year');
+        }
+        else{
+          if(this.state.classSection.length > 1){
+            alert('Pleas insert a correct class letter');
+          }
+          else{
+            var letters = /^[a-zA-Z]+$/;
+            if(!this.state.classSection.match(letters)){
+              alert('Pleas insert a correct class letter');
+            }
+            else{
+              if(d2 > d){ //DEBUG: set condition to false to bypass it
+                alert("You cannot record a future date");
+                event.preventDefault(); 
+                return; 
+              }
+              else{
+                if(d2 < lastSunday){
+                  alert("It is too late to insert record for the selected");
+                  event.preventDefault(); 
+                  return;
+                }
+                else{
+                  //END of validation. Update the interface, store data in db here
+                  //Update state and interface
+                  this.props.addLecture(this.state.subject, this.state.date, this.state.classYear+this.state.classSection, this.state.summary);
+                }
+              }
+            }
+          }
+        }
+      }
+  }
     event.preventDefault();
   }
 
@@ -51,8 +89,11 @@ class DailyLecture extends Component {
         <form className="form" action= "" onSubmit= { values => this.validateData(values) }>
          Date: <input type= "date" name= "date" className= "dlDate" onChange= { (event) => this.setState({date: event.target.value})} /> <br/>
          Subject: <input type= "text" name="subject" className= "dlSubject" onChange= { (event) => this.setState({subject: event.target.value})} /><br/><br/>
-          <textarea rows= "4" cols="50" placeholder= "Fill lecture summary here" onChange= { (event) => this.setState({summary: event.target.value})} ></textarea><br/><br/>
-          <input type="submit" value= "Send"/>
+         Class Year: <input type="text" name="class" className= "dlClassYear" onChange={ (event) => this.setState({classYear: event.target.value})}></input>
+         Class Section: <input type="text" name="class" className= "dlClassSection" onChange={ (event) => this.setState({classSection: event.target.value})}></input>
+          <textarea rows= "4" cols="50" className="formTextArea" placeholder= "Fill lecture summary here" onChange= { (event) => this.setState({summary: event.target.value})} ></textarea><br/><br/>
+          <input type="submit" value= "Send" className="submitButton"/>
+          <input type="reset" value="Cancel" className="cancelButton" onClick={() => this.props.cancelAddTopic()}/>
         </form>
       </div>
     );
--- frontend/src/components/teacher/components/TeacherLectureSummary/index.jsx
+++ frontend/src/components/teacher/components/TeacherLectureSummary/index.jsx
@@ -1,38 +1,55 @@
 import React, { Component } from 'react';
+import DailyLecture from '../DailyLecture/index';
 import './style.sass';
 
 class TeacherLectureSummary extends Component {
 
-  state = { 
-    summary: "",
-    edit: false,
-    modifySubject: "",
-    modifyDate: "",
-    modifyClass: "",
-    modifySummary: "",
-  };
+  constructor(props){
+    super(props);
+    this.state = { 
+      summary: "",
+      showMore: false,
+      edit: false,
+      addTopicRequest: false,
+      modifySubject: "",
+      modifyDate: "",
+      modifyClass: "",
+      modifySummary: "",
+      lectureInfo: [
+        {
+          subject: "Chemistry",
+          date: "09/24/2019",
+          class: "5B",
+          summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" ,       
+        },
+        {
+          subject: "Physics",
+          date: "10/27/2019",
+          class: "1A",
+          summary: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        },
+        {
+          subject: "Geography",
+          date: "11/04/2019",
+          class: "3C",
+          summary: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+        },
+    
+      ]
+    };
+    this.cancelAddTopic = this.cancelAddTopic.bind(this);
+    this.addLectureToSummary = this.addLectureToSummary.bind(this);
+  }
 
-  lectureInfo = [
-    {
-      subject: "Chemistry",
-      date: "09/24/2019",
-      class: "5B",
-      summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" ,       
-    },
-    {
-      subject: "Physics",
-      date: "10/27/2019",
-      class: "1A",
-      summary: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    },
-    {
-      subject: "Geography",
-      date: "11/04/2019",
-      class: "3C",
-      summary: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-    },
-
-  ];
+  addLectureToSummary(subject, date, clas, resume){
+    alert(subject + date + clas + resume);
+    this.setState({addTopicRequest: false, lectureInfo: this.state.lectureInfo.concat({
+      subject: subject,
+      date: date,
+      class: clas,
+      summary: resume
+    })});
+  }
 
   renderTableItem = (item, index) => {
     return (
@@ -40,7 +57,7 @@ class TeacherLectureSummary extends Component {
         <td className= "columnItem">{item.subject}</td>
         <td className= "columnItem">{item.date}</td>
         <td className= "columnItemClass">{item.class}</td>
-        <td><span className= "moreButton" onClick = { () => this.setState({ summary: item.summary, edit: false }) }>More</span><span className="editButton" onClick= { (event ) => this.validateEdit(event, item.subject, item.date, item.class, item.summary) } >Edit</span></td>
+        <td><span className= "moreButton" onClick = { () => this.setState({ summary: item.summary, edit: false, showMore: true }) }>More</span><span className="editButton" onClick= { (event ) => this.validateEdit(event, item.subject, item.date, item.class, item.summary) } >Edit</span></td>
       </tr>
     );
   }
@@ -81,6 +98,10 @@ class TeacherLectureSummary extends Component {
     event.preventDefault();
   }
 
+  cancelAddTopic(){
+    this.setState({addTopicRequest: false, showMore: false});
+  }
+
    render() {
     
     return (
@@ -92,9 +113,13 @@ class TeacherLectureSummary extends Component {
             <th className="columnName">Class</th>
             <th className="columnName">Summary</th>
           </tr>
-          { this.lectureInfo.map(this.renderTableItem) }
+          {this.state.lectureInfo.map(this.renderTableItem) }
+          <span className="addTopicButton" onClick={() => this.setState({showMore: false, addTopicRequest: true}) }>Add Topic</span>
         </table>
-        { this.checkEdit() } 
+        { this.state.showMore === true && this.state.addTopicRequest === false && this.checkEdit()}
+        { this.state.addTopicRequest === true && (
+          <DailyLecture cancelAddTopic = {this.cancelAddTopic} addLecture = {this.addLectureToSummary}/>
+        )} 
       </div>
     )
   }
