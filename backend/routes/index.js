ece4654c838b5ea9d94acdb881bbbc90c2312f78
Emile <legendre.emile.27@gmail.com>
2019-11-12 13:51:22 +0100

    Add a route to query the children of a parent

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,12 @@ const auth = require('./auth');
 const grades = require('./grades');
 const lectures = require('./lectures');
 const admin = require('./admin');
+const parent = require('./parent');
 
 module.exports = [
     ...auth,
     ...grades,
     ...lectures,
     ...admin,
+    ...parent
 ];
