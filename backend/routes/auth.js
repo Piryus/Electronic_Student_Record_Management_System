142f14c2560ba5b042f3616c56fd100ddd8f7205
Emile <legendre.emile.27@gmail.com>
2019-11-12 21:43:12 +0100

    Update login route with scope in response and children if user is parent

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const Joi = require('@hapi/joi');
-
+const Boom = require('boom');
 const User = require('../models/User');
+const Parent = require('../models/Parent');
+const Student = require('../models/Student');
 
 const login = async function (request, h) {
     try {
@@ -14,7 +16,32 @@ const login = async function (request, h) {
             request.cookieAuth.set({
                 id: user._id
             });
-            return {success: true};
+            // Case: user is a parent
+            if (user.scope.includes('parent')) {
+                // Retrieves the parent in the DB using the user ID
+                const parent = await Parent.findOne({userId: user._id});
+                // If the parent isn't found, it shouldn't happen, throw an error
+                if (parent === null) {
+                    return Boom.badRequest();
+                }
+                // Retrieves the parent's children using the parent's children's IDs
+                const children = await Student.find({'_id': { $in: parent.children}});
+                // For each children, keep only the necessary data and push those in an array
+                let childrenFiltered = [];
+                for (const childKey in children) {
+                    let childFiltered = {
+                        _id: children[childKey]._id,
+                        ssn: children[childKey].ssn,
+                        name: children[childKey].name,
+                        surname: children[childKey].surname
+                    };
+                    childrenFiltered.push(childFiltered);
+                }
+                // Returns successful login, scope and parent's children
+                return {success: true, scope: user.scope, children: childrenFiltered};
+            }
+            // Returns successful login and scope (admin or teacher)
+            return {success: true, scope: user.scope};
         }
     } catch (err) {
         return Boom.badImplementation(err);
