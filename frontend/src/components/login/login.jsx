a9a5c3b17b2fee230539bbe093b4e33ae6bcec55
Emile <legendre.emile.27@gmail.com>
2019-11-15 16:38:51 +0100

    Update login style

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './login.sass';
+import styles from './login.module.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
@@ -59,24 +59,27 @@ class Login extends Component {
     render() {
         let error;
         if (this.state.error) {
-            error = <Alert variant={'danger'} >Sorry, the mail or password was incorrect.</Alert>
+            error = <Alert variant={'danger'}>Sorry, the mail or password was incorrect.</Alert>
         }
         if (this.state.unknownError) {
-            error = <Alert variant={'danger'} >Sorry, an unknown error happened... It's not you, it's us.</Alert>
+            error = <Alert variant={'danger'}>Sorry, an unknown error happened... It's not you, it's us.</Alert>
         }
-        return (<div className="loginContainer">
-                <h2 className={'loginText'}>Log in to continue</h2>
-                {error}
-                <Form onSubmit={event => {
-                    event.preventDefault();
-                    this.handleLogin(this.state.username, this.state.password);
-                }}>
-                    <Form.Control value={this.state.username} type="email" placeholder="Email address"
-                                  onChange={(e) => this.setState({username: e.target.value})}/>
-                    <Form.Control value={this.state.password} type="password" name="password" placeholder="Password"
-                                  onChange={(e) => this.setState({password: e.target.value})} required/>
-                    <Button disabled={!this.validateForm()} type="submit" block>Log in</Button>
-                </Form>
+        return (
+            <div className={styles.root}>
+                <div className={styles.loginContainer}>
+                    <h2 className={styles.loginText}>Log in to continue</h2>
+                    {error}
+                    <Form onSubmit={event => {
+                        event.preventDefault();
+                        this.handleLogin(this.state.username, this.state.password);
+                    }}>
+                        <Form.Control value={this.state.username} type="email" placeholder="Email address"
+                                      onChange={(e) => this.setState({username: e.target.value})}/>
+                        <Form.Control value={this.state.password} type="password" name="password" placeholder="Password"
+                                      onChange={(e) => this.setState({password: e.target.value})} required/>
+                        <Button disabled={!this.validateForm()} type="submit" block>Log in</Button>
+                    </Form>
+                </div>
             </div>
         );
     }
