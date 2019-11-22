999c767edee12675fc1d89d6798e7a7d11b91c08
antoniocastoS262622 <s262622@studenti.polito.it>
2019-11-22 17:06:36 +0100

    [Story #6] Removed handler to get subjects for a teacher

--- backend/routes/teacher.js
+++ backend/routes/teacher.js
@@ -5,36 +5,8 @@ const Utils = require('../utils');
 const Valid = require('../validation');
 const Teacher = require('../models/Teacher');
 
-const getTeacherSubjects = async function(request, h){
-    try{
-        const {id} = request.params;
-        const teacher = await Teacher.findOne({_id: id});
-        if(teacher === null){
-            return Boom.badRequest();
-        }
-        return { subjects: teacher.subjects };
-    } catch(err){
-        return Boom.badImplementation(err);
-    }
-};
-
 const routes = [
-    {
-        method: 'GET',
-        path: '/subjects/{id}',
-        handler: getTeacherSubjects,
-        options: {
-            auth: {
-                strategy: 'session',
-                scope: 'teacher'
-            },
-            validate: {
-                params: {
-                    id: Valid.id.required()
-                }
-             }
-        },
-    }
+    
 ];
 
 module.exports = routes;
