8677710a5cc791f8178a3900e68eb861b4b3e7a0
Emile <legendre.emile.27@gmail.com>
2019-11-26 00:03:06 +0100

    Make teacher page more responsive

--- frontend/src/components/teacher/teacher.js
+++ frontend/src/components/teacher/teacher.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css';
-import {Container, Row, Nav, Navbar, Button} from 'react-bootstrap';
-import {FaGraduationCap, FaCog, FaMedal, FaBook} from 'react-icons/fa'
+import {Container, Nav, Row} from 'react-bootstrap';
+import {FaBook, FaCog, FaGraduationCap, FaMedal} from 'react-icons/fa'
 import LectureTopics from './lecture-topics';
 import StudentGradesSummary from './student-grades-summary/studentGradesSummary';
 import Assignments from './assignments/assignments';
@@ -11,12 +11,11 @@ import AppNavbar from "../common-components/navbar/navbar";
 export default class Teacher extends React.Component {
 
 
-
     constructor(props) {
         super(props);
 
         let subjects = [];
-        this.props.timetable.forEach((value) =>{
+        this.props.timetable.forEach((value) => {
             subjects[value.subject] = value.subject;
         });
 
@@ -28,13 +27,13 @@ export default class Teacher extends React.Component {
         };
     }
 
-    async componentDidMount(){
-            //Load all students
-            await this.getAllStudents();
+    async componentDidMount() {
+        //Load all students
+        await this.getAllStudents();
     }
 
-    async getAllStudents(){
-        try{
+    async getAllStudents() {
+        try {
             const url = 'http://localhost:3000/students/all';
             const options = {
                 method: 'GET',
@@ -49,15 +48,10 @@ export default class Teacher extends React.Component {
             this.setState({
                 students: json.students,
             });
-        } catch(e){
+        } catch (e) {
             alert(e);
         }
     }
-
-
-
-
-
 
     setUserRequest(e, choice) {
         this.setState({userRequest: choice});
@@ -65,28 +59,38 @@ export default class Teacher extends React.Component {
 
     render() {
         const now = new Date();
-        
-
         return (
             <div className={styles.root}>
-                <AppNavbar type='classic' onLogout={() => this.props.onLogout()} />)}
+                <AppNavbar type='classic' onLogout={() => this.props.onLogout()} onHamburgerMenu={() => this.setState({sidebarOpen: !this.state.sidebarOpen})}/>
                 <Container fluid>
                     <Row>
-                        <Nav className={["flex-column bg-light col-md-2 d-none d-md-block", styles.sidebar]}>
-                            <Nav.Link className={this.state.userRequest === 'lecture' ? styles.sidebarLinkActive : styles.sidebarLink} onClick={(e) => this.setUserRequest(e, "lecture")}><FaGraduationCap/> Lecture Topics</Nav.Link>
-                            <Nav.Link className={this.state.userRequest === 'grades' ? styles.sidebarLinkActive : styles.sidebarLink} onClick={(e) => this.setUserRequest(e, "grades")}><FaMedal/> Student Grades</Nav.Link>
-                            <Nav.Link className={this.state.userRequest === 'assignments' ? styles.sidebarLinkActive : styles.sidebarLink} onClick={(e) => this.setUserRequest(e, "assignments")}><FaBook/> Assignments </Nav.Link>
-                            <Nav.Link className={this.state.userRequest === 'settings' ? styles.sidebarLinkActive : styles.sidebarLink} onClick={(e) => this.setUserRequest(e, "settings")} ><FaCog/> Settings</Nav.Link>
+                        <Nav
+                            className={[this.state.sidebarOpen ? 'bg-light col-5' : 'd-none', "flex-column bg-light col-md-2 d-md-block", styles.sidebar]}>
+                            <Nav.Link
+                                className={this.state.userRequest === 'lecture' ? styles.sidebarLinkActive : styles.sidebarLink}
+                                onClick={(e) => this.setUserRequest(e, "lecture")}><FaGraduationCap/> Lecture
+                                topics</Nav.Link>
+                            <Nav.Link
+                                className={this.state.userRequest === 'grades' ? styles.sidebarLinkActive : styles.sidebarLink}
+                                onClick={(e) => this.setUserRequest(e, "grades")}><FaMedal/> Student grades</Nav.Link>
+                            <Nav.Link
+                                className={this.state.userRequest === 'assignments' ? styles.sidebarLinkActive : styles.sidebarLink}
+                                onClick={(e) => this.setUserRequest(e, "assignments")}><FaBook/> Assignments </Nav.Link>
+                            <Nav.Link
+                                className={this.state.userRequest === 'settings' ? styles.sidebarLinkActive : styles.sidebarLink}
+                                onClick={(e) => this.setUserRequest(e, "settings")}><FaCog/> Settings</Nav.Link>
                         </Nav>
-                        <main className={"col-md-9 ml-sm-auto col-lg-10 px-4 pt-5"}>
-                            {this.state.userRequest === 'lecture' && (<LectureTopics timetable={this.props.timetable.reduce((obj, x) => {
-                                obj[x.weekhour] = { name: x.subject, active: lib.weekhourToDate(x.weekhour) < now };
-                                return obj;
-                            }, {})}/>)}
-                             {this.state.userRequest === 'grades' && (
-                                <StudentGradesSummary students={this.state.students} subjects={this.state.subjects} type='teacher-grades'/>
+                        <main className={"col-md-10 ml-sm-auto col-lg-10 px-4 pt-5"}>
+                            {this.state.userRequest === 'lecture' && (
+                                <LectureTopics timetable={this.props.timetable.reduce((obj, x) => {
+                                    obj[x.weekhour] = {name: x.subject, active: lib.weekhourToDate(x.weekhour) < now};
+                                    return obj;
+                                }, {})}/>)}
+                            {this.state.userRequest === 'grades' && (
+                                <StudentGradesSummary students={this.state.students} subjects={this.state.subjects}
+                                                      type='teacher-grades'/>
                             )}
-                            {this.state.userRequest === 'assignments' &&(
+                            {this.state.userRequest === 'assignments' && (
                                 <Assignments subjects={this.state.subjects} timetable={this.props.timetable}/>
                             )}
                         </main>
