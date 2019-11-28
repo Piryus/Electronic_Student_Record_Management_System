52f60b0ddf84f4022c5c54be255e4e5d363e5d3e
Emile <legendre.emile.27@gmail.com>
2019-11-28 19:03:36 +0100

    [Front] Implement delete user button

--- frontend/src/components/admin/accounts-management/delete-user-modal/delete-user-modal.js
+++ frontend/src/components/admin/accounts-management/delete-user-modal/delete-user-modal.js
@@ -1,23 +1,50 @@
 import React from 'react';
-import {Button, Modal} from "react-bootstrap";
+import {Alert, Button, Modal} from "react-bootstrap";
 
 export default class DeleteUserModal extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            error: false
+        }
     }
 
-    deleteUser() {
-        // Use this.props.user
-        // TODO Call backend to delete user
-        this.props.handleClose();
+    async deleteUser() {
+        try {
+            const url = 'http://localhost:3000/users/' + this.props.user._id;
+            const options = {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include'
+            };
+            const response = await fetch(url, options);
+            const responseJson = await response.json();
+            if (responseJson.success) {
+                this.props.handleClose(true);
+            } else {
+                this.setState({error: true});
+            }
+        } catch (e) {
+            this.setState({error: true});
+        }
+
     }
 
     render() {
-        return(
+        return (
             <Modal show={this.props.show} onHide={this.props.handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title className="text-center">Are you sure you want to delete {this.props.user.name} {this.props.user.surname}?</Modal.Title>
+                    <Modal.Title className="text-center">Are you sure you want to
+                        delete {this.props.user.name} {this.props.user.surname}?</Modal.Title>
                 </Modal.Header>
+                {this.state.error && (
+                    <Modal.Body>
+                        <Alert variant='danger'>Unable to delete this user.</Alert>
+                    </Modal.Body>
+                )}
                 <Modal.Footer>
                     <Button variant="success" onClick={() => this.deleteUser()}>Delete</Button>
                     <Button variant="danger" onClick={this.props.handleClose}>Cancel</Button>
