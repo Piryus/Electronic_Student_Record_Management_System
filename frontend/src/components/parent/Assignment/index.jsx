53c6c124e04a76364f6107c7f6eee18e3ce83e6b
Mieye Ombe <mieye@usophy.com>
2019-11-29 01:29:18 +0100

    Assignment sorted by date

--- frontend/src/components/parent/Assignment/index.jsx
+++ frontend/src/components/parent/Assignment/index.jsx
@@ -12,39 +12,6 @@ class Assignment extends Component {
     }
   }
 
-  tmp = [
-    {
-      subject: "Physics",
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-      assigned: "10/11/2019",
-      due: "20/11/2019",
-    },
-    {
-      subject: "Chemistry",
-      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      assigned: "18/11/2019",
-      due: "24/11/2019",
-    },
-    {
-      subject: "Biology",
-      description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      assigned: "22/11/2019",
-      due: "30/11/2019",
-    },
-    {
-      subject: "Geography",
-      description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      assigned: "13/11/2019",
-      due: "24/11/2019",
-    },
-    {
-      subject: "Italian",
-      description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      assigned: "12/11/2019",
-      due: "25/11/2019",
-    },
-  ];
-
    async componentDidMount() {
     await this.getChildAssignment();
   } 
@@ -90,23 +57,15 @@ class Assignment extends Component {
      let store= this.state.childAssignment;
     if (Array.isArray(store) && store.length) {
       store.sort(function(a, b){
-        let a1= a.due.split("/");
-        temp= a1[0];
-        a1[0]= a1[2];
-        a1[2]= temp;
-        //a1= a1[2] + "," + a1[1] + "," + a1[0];
-        let d1= new Date(a1);
-        let b1= b.due.split("/");
-        //b1= b1[2] + "," + b1[1] + "," + b1[0];
-        temp= b1[0];
-        b1[0]= b1[2];
-        b1[2]= temp;
-        let d2= new Date(b1);
-
-        return (d1.getTime() - d2.getTime())
-        //return (a.getTime() - b.getTime()) for db data
-
+        return new Date(a) - new Date(b);
       });
+      store.map((item)=>{
+        let tmp= [];
+        tmp= item.due.split("T");
+        item.due= tmp[0];
+        tmp= item.assigned.split("T");
+        item.assigned= tmp[0];
+      })
       output= (
       <Accordion>
          <Card>
@@ -118,14 +77,14 @@ class Assignment extends Component {
        </Accordion>
       ) 
     }
-    else{
+    else{/*
       output= <h3 style= {{color: "red"}}>You currently have no pending assignments!!!!</h3>
-    }
+    */}
     
     
     return (
       <div style= {{width: "80%", margin: "0 auto"}}>
-        <h2>Assignments</h2>
+        <h2 style= {{textAlign: "center"}}>Assignments</h2>
         <br /><br />
        { output }
     </div>
