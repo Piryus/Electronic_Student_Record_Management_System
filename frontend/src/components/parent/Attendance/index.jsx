cc63ca6784ee77579feb845cf5ebe686907863ab
Emile <legendre.emile.27@gmail.com>
2019-11-30 00:46:08 +0100

    [Front] Continue simplifying attendance management

--- frontend/src/components/parent/Attendance/index.jsx
+++ frontend/src/components/parent/Attendance/index.jsx
@@ -1,299 +1,334 @@
-import React, { Component } from 'react';
-
-
+import React from 'react';
 import MyTimetable from '../../utils/MyTimetable';
+import SectionHeader from "../../utils/section-header";
+import {Container} from "react-bootstrap";
 
-const absent= "Absent";
-const empty= "";
-const earlyExit= "Early Exit";
-const lateEntrance= "Late Entrance";
-const lateAndEarly= <span>Late Entry <br/> Early Exit</span>;
-var tmp2= ["a", "b", "c", "d", "e", "f",];
-var tmp3= [
-  {
-    date: "",
-    content: [
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-    ]
-  },
-  {
-    date: "",
-    content: [
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-    ]
-  },
-  {
-    date: "",
-    content: [
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-    ]
-  },
-  {
-    date: "",
-    content: [
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-    ] 
-  },
-  {
-    date: "",
-    content: [
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-      {
-        text: "",
-        color: ""
-      },
-    ]
-    
-  }
-];
-const tmp= [
-  {
-    date: "25/11/2019",
-    enter: 1,
-    exit: 5,
-    event: {
-      absent: false,
-      lateEntrance: false,
-      earlyExit: true,
-    }, 
-    
-  },
-  {
-    date: "26/11/2019",
-    enter: 1,
-    exit: 4,
-    event: {
-      absent: false,
-      lateEntrance: false,
-      earlyExit: false,
+const absent = "Absent";
+const empty = "";
+const earlyExit = "Left early";
+const lateEntrance = "Arrived late";
+const lateAndEarly = <span>Late Entry <br/> Early Exit</span>;
+var tmp2 = ["a", "b", "c", "d", "e", "f",];
+var tmp3 = [
+    {
+        date: "",
+        content: [
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+        ]
     },
-    
-  },
-  {
-    date: "27/11/2019",
-    enter: 1,
-    exit: 6,
-    event: {
-      absent: true,
-      lateEntrance: false,
-      earlyExit: false,
+    {
+        date: "",
+        content: [
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+        ]
     },
-    
-  },
-  {
-    date: "28/11/2019",
-    enter: 1,
-    exit: 5,
-    event: {
-      absent: false,
-      lateEntrance: true,
-      earlyExit: false,
+    {
+        date: "",
+        content: [
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+        ]
     },
-    
-  },
-  {
-    date: "29/11/2019",
-    enter: 1,
-    exit: 5,
-    event: {
-      absent: false,
-      lateEntrance: false,
-      earlyExit: false,
+    {
+        date: "",
+        content: [
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+        ]
     },
-    
-  },
-];
-   
-class Attendance extends Component {
+    {
+        date: "",
+        content: [
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+            {
+                text: "",
+                color: ""
+            },
+        ]
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      childAttendance: [],
     }
-  }
-
-  startOfWeek = (date) =>
-  {
-    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
-  
-    return new Date(date.setDate(diff));
- 
-  }//To get first monday of the week for weekly query
-
-  async componentDidMount() {
-    await this.getChildAttendance();
-  } 
-  
-  async getChildAttendance() {
-    // Query child's attendance
-    const url = 'http://localhost:3000/attendance/' + this.props.child._id;
-    const options = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
+];
+const tmp = [
+    {
+        date: "25/11/2019",
+        enter: 1,
+        exit: 5,
+        event: {
+            absent: false,
+            lateEntrance: false,
+            earlyExit: true,
         },
-        credentials: 'include',
+
+    },
+    {
+        date: "26/11/2019",
+        enter: 1,
+        exit: 4,
+        event: {
+            absent: false,
+            lateEntrance: false,
+            earlyExit: false,
+        },
+
+    },
+    {
+        date: "27/11/2019",
+        enter: 1,
+        exit: 6,
+        event: {
+            absent: true,
+            lateEntrance: false,
+            earlyExit: false,
+        },
+
+    },
+    {
+        date: "28/11/2019",
+        enter: 1,
+        exit: 5,
+        event: {
+            absent: false,
+            lateEntrance: true,
+            earlyExit: false,
+        },
+
+    },
+    {
+        date: "29/11/2019",
+        enter: 1,
+        exit: 5,
+        event: {
+            absent: false,
+            lateEntrance: false,
+            earlyExit: false,
+        },
+
+    },
+];
+
+export default class Attendance extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            childAttendance: [],
+        }
+    }
+
+    // Get first monday of the week based on passed date
+    startOfWeek = (date) => {
+        const diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+        return new Date(date.setDate(diff));
     };
-    let response = await fetch(url, options);
-    const json = await response.json();
-    this.setState({
-       childAttendance: json.attendance
-    });
-  }
 
-  render() {
-    //tmp equals GET data
-    //Handle data from backend
-    let store= this.state.childAssignment || tmp;
-    const now = new Date();
-    const sw= this.startOfWeek(now);//for weekly query
-    
-    return (
-      <div style= {{width: "80%", margin: "0 auto"}}>
-        <h1 style= {{textAlign: "center"}}>Daily Lectures Attendance</h1>
-        <br/><br/> 
-          {store.map((item, index) => {
-            tmp3[index].date= tmp[index].date;
-                {tmp2.map((it, i) => {
-                    if(item.event.absent){
-                      tmp3[index].content[i].color = "#996633";
-                      tmp3[index].content[i].text = absent;
-                      }
-                      else if(item.event.lateEntrance && i == item.enter - 1){
-                        tmp3[index].content[i].color = "#248f24";
-                        tmp3[index].content[i].text = lateEntrance; 
-                      }
-                      else if(item.event.earlyExit && i == item.exit - 1){
-                        tmp3[index].content[i].color = "#248f24";
-                        tmp3[index].content[i].text = earlyExit; 
-                      }
-                      else if(i >= item.enter - 1 && i <= item.exit -1){
-                        tmp3[index].content[i].color = "#248f24";
-                        tmp3[index].content[i].text = empty; 
-                      }
-                      else{
-                        tmp3[index].content[i].color = "#e0ebeb";
-                        tmp3[index].content[i].text = empty;
-                      }
-                })}
-            })}
-        <MyTimetable data={tmp3} />
-      </div>
-    )
-  }
+    async componentDidMount() {
+        await this.getChildAttendance();
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        await this.getChildAttendance();
+    }
+
+    async getChildAttendance() {
+        // Query child's attendance
+        const url = 'http://localhost:3000/attendance/' + this.props.child._id;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        };
+        const response = await fetch(url, options);
+        const json = await response.json();
+        if (this.state.childAttendance !== json.attendance) {
+            this.setState({
+                childAttendance: json.attendance
+            });
+        }
+    }
+
+    render() {
+        console.log(this.state.childAttendance);
+        // //Handle data from backend
+        // let store = this.state.childAssignment || tmp;
+        // const now = new Date();
+        // const sw = this.startOfWeek(now);//for weekly query
+
+        // Basic data construction
+        let data = [];
+        for (let dayIndex = 0; dayIndex < 5; dayIndex++) {
+            let dateObject = {};
+            dateObject.date = new Date(this.startOfWeek(new Date())); // TODO Use hlib
+            dateObject.date.setDate(dateObject.date.getDate() + dayIndex);
+            let content = [];
+            for (let hourIndex = 0; hourIndex < 6; hourIndex++) {
+                content.push({
+                    text: '',
+                    color: 'bg-success text-white'
+                });
+            }
+            dateObject.content = content;
+            data.push(dateObject);
+        }
+
+        this.state.childAttendance.forEach((event, index) => {
+            const eventDate = new Date(event.date);
+            data.forEach(day => {
+                if (eventDate.getDate() === day.date.getDate() &&
+                    eventDate.getMonth() === day.date.getMonth() &&
+                    eventDate.getFullYear() === day.date.getFullYear()) {
+                    if (event.event === 'absence') {
+                        day.content.forEach((hour => {
+                            hour.color = 'bg-danger text-white';
+                            hour.text = 'Absent';
+                        }))
+                    } else if (event.event === 'late-entry') {
+                      // TODO Manage this case
+                    } else if (event.event === 'early-exit') {
+                      // TODO Manage this case
+                    }
+                }
+            });
+        });
+
+        return (
+            <Container fluid>
+                <SectionHeader>Attendance</SectionHeader>
+                {/*{this.state.childAttendance.map((event, index) => {*/}
+                {/*    {*/}
+                {/*        tmp2.map((it, i) => {*/}
+                {/*            if (item.event.absent) {*/}
+                {/*                tmp3[index].content[i].color = 'bg-danger text-white';*/}
+                {/*                tmp3[index].content[i].text = absent;*/}
+                {/*            } else if (item.event.lateEntrance && i == item.enter - 1) {*/}
+                {/*                tmp3[index].content[i].color = 'bg-warning text-white';*/}
+                {/*                tmp3[index].content[i].text = lateEntrance;*/}
+                {/*            } else if (item.event.earlyExit && i == item.exit - 1) {*/}
+                {/*                tmp3[index].content[i].color = 'bg-warning';*/}
+                {/*                tmp3[index].content[i].text = earlyExit;*/}
+                {/*            } else if (i >= item.enter - 1 && i <= item.exit - 1) {*/}
+                {/*                tmp3[index].content[i].color = 'bg-success text-white';*/}
+                {/*                tmp3[index].content[i].text = empty;*/}
+                {/*            } else {*/}
+                {/*                tmp3[index].content[i].color = 'bg-secondary';*/}
+                {/*                tmp3[index].content[i].text = empty;*/}
+                {/*            }*/}
+                {/*        })*/}
+                {/*    }*/}
+                {/*})}*/}
+                <MyTimetable data={data}/>
+            </Container>
+        );
+    }
 }
-
-export default Attendance;
