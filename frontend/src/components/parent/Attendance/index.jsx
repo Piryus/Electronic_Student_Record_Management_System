2a5c18570e5beee0bc9bfcba73788a1616c6c7b3
Emile <legendre.emile.27@gmail.com>
2019-11-30 10:42:43 +0100

    [Front] Delete deprecated SASS file

--- frontend/src/components/parent/Attendance/index.jsx
+++ frontend/src/components/parent/Attendance/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MyTimetable from '../../utils/MyTimetable';
+import MyTimetable from '../../utils/my-timetable';
 import SectionHeader from "../../utils/section-header";
 import {Container} from "react-bootstrap";
 
@@ -9,6 +9,7 @@ export default class Attendance extends React.Component {
         super(props);
         this.state = {
             childAttendance: [],
+
         }
     }
 
