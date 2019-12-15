20526bc9c086faef7a9bc6ce2b1e54ca70e43061
emarkk <ema9628@gmail.com>
2019-12-15 12:31:21 +0100

    [Back] Refactor publishTimetables

--- backend/routes/secretary/handlers.js
+++ backend/routes/secretary/handlers.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-
 const Boom = require('boom');
 const HLib = require('@emarkk/hlib');
 
@@ -64,45 +62,17 @@ const sendCredentials = async function(parents) {
 };
 
 const publishTimetables = async function(timetablesFile) {
-    const CLASS_SEPARATOR = '@', TEACHER_SUBJECT_SEPARATOR = ':';
     const schoolClasses = await SchoolClass.find();
     let teachers = await Teacher.find().populate('userId');
 
+    const timetableEntries = HLib.parseTimetablesFile(timetablesFile, schoolClasses, teachers);
+
     teachers = teachers.map(t => {
-        t.timetable = [];
+        t.timetable = timetableEntries.filter(e => e.teacherId.equals(t._id)).map(e => {
+            return { classId: e.classId, subject: e.subject, weekhour: e.weekhour };
+        })
         return t;
     });
-
-    if(timetablesFile.headers['content-type'] !== 'text/csv')
-        return Boom.badRequest();
-
-    const input = fs.readFileSync(timetablesFile.path).toString().replace(/\r/g, '');
-
-    if(input.indexOf(CLASS_SEPARATOR) !== -1)
-        return Boom.badRequest();
-    
-    const classes = input.replace(/\n(\n)+/, CLASS_SEPARATOR).split(CLASS_SEPARATOR);
-
-    for(let c of classes) {
-        const data = c.split('\n').filter(l => l !== '').map(l => l.split('\t'));
-
-        if(![7, 8].includes(data.length) || data[0][0] !== 'Class' || data[1].join() != 'Monday,Tuesday,Wednesday,Thursday,Friday')
-            return Boom.badRequest();
-            
-        const classId = (schoolClasses.find(sc => sc.name === data[0][1]) || {})._id;
-
-        if(classId === undefined)
-            return Boom.badRequest();
-
-        for(let i = 2; i < data.length; i++) {
-            for(let j = 0; j < data[i].length; j++) {
-                const [subject, teacherSsn] = data[i][j].split(TEACHER_SUBJECT_SEPARATOR);
-                const t = teachers.findIndex(t => t.userId.ssn === teacherSsn);
-                if(t !== -1)
-                    teachers[t].timetable.push({ classId, subject, weekhour: [j, i-2].join('_') });
-            }
-        }
-    }
 
     await Promise.all(teachers.map(t => t.save()));
 
